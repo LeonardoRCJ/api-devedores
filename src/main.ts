e6849e743c51593e09d9ea8c596af3207916d3c9
Leonardo Rocha Cavalcante <leonardorocha790@gmail.com>
2025-08-28 19:32:34 -0300

    feat: enabling cors at the api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.enableCors({
+    origin: '*', // Permite pedidos de qualquer origem. Para desenvolvimento, isto é seguro.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
